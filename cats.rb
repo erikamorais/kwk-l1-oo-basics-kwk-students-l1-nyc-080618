2c70dc79e00eb380d2e4de8d3b394b6c60603833
erikamorais <erikamorais2001@gmail.com>
2018-08-10 19:34:12 +0000

    automatically backed up by learn

---
+++ cats.rb
@@ -0,0 +1,14 @@
+class Cats
+  
+  def initialize (cute, fuzzy, mean, cuddly)
+    @cute = cute 
+    @fuzzy = fuzzy 
+    @mean = mean
+  end 
+  
+end 
+  
+  tiger = Cats.new ("Oh it's super snazzy", "pretty fuzzy", "super duper mean", "absolutly not")
+  
+  garfield = Cats.new ("ewwww", "yeah... kinda", "well... he kinda hates mondays", "no")
+  
